36142a0245b4273d989d405b711911a86dee06de
czw <8998799@qq.com>
2018-11-23 15:34:39 +0800

    index

---
+++ src/components/commons/Header.vue
@@ -0,0 +1,26 @@
+<template>
+	<div class="head">
+		{{test}}
+	</div>
+</template>
+
+<script type="text/javascript">
+	export default {
+		name: "Header",
+		data(){
+			return {
+				test: "Head"
+			}
+		},
+	}
+</script>
+
+<style lang="less" scoped>
+	@import url('../../styles/main.less');
+	.head{
+		.h(54);
+		width:100%;
+		background-color: #f12e24;
+		
+	}
+</style>
