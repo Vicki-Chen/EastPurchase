2fd5782157925fa126b539920b4e0b18a8862348
czw <8998799@qq.com>
2018-11-23 14:26:35 +0800

    搭建框架

---
+++ src/components/pages/Home/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home">
+    {{msg}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+      msg: 'Home'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+@import url('../../../styles/main.less');
+.home{
+  color:red;
+}
+</style>
---
+++ src/store/index.js
@@ -0,0 +1,32 @@
+import Vue from 'vue'// vue实例
+import Vuex from 'vuex' //vue router
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+    state:{        //state  状仓库  所有的状态值放在这里管理
+        login:window.localStorage.getItem("login") || false, 
+        introduce:'http://www.dankegongyu.com/zhuanti/trade?city=bj&amp;trade=hlg', 
+        citypos:'', 
+        cityid:'1',
+        city:"北京",
+    },
+     mutations:{    //mutations：修改仓库中状态值的方法 ，state的修改只有通过mutations才能修改
+    //更改state 里的数据的方法
+        changeLogin(state,val1){
+            state.login=val1
+        },
+        changeIntroduce(state,val1){
+            state.introduce=val1
+        },
+        changeCityPos(state,val1){
+            state.citypos=val1
+        },
+        changeCityId(state,val1){
+            state.cityid=val1
+        },
+         changeCity(state,val1){
+            state.city=val1
+        },
+    }
+})
