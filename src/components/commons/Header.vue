3653043bf83e05222c237cc1813186f68c48c47a
czw <8998799@qq.com>
2018-11-23 16:08:15 +0800

    index

--- src/components/commons/Header.vue
+++ src/components/commons/Header.vue
@@ -1,6 +1,9 @@
 <template>
 	<div class="head">
-		{{test}}
+		<i class="fa fa-bars" aria-hidden="true"></i>
+		<img src="http://m.ocj.com.cn/common/mobile_phone/cssimage/indeximg/logoicon.png" />
+		<input type="" name="" value="搜索货号/关键词">
+		<i class="fa fa-search" aria-hidden="true"></i>
 	</div>
 </template>
 
@@ -18,9 +21,33 @@
 <style lang="less" scoped>
 	@import url('../../styles/main.less');
 	.head{
+		position: relative;
+		.fs(24);
+		color: #fff;
+		display: flex;
+		justify-content: space-around;
+		align-items: center;
 		.h(54);
 		width:100%;
 		background-color: #f12e24;
-		
+		img{
+			.w(35);
+			.h(35)
+		}
+		input{
+			.w(267);
+			.h(32);
+			color: #fff;
+			background-color: #d42e2c;
+		    border: 1px solid #d42e2c;
+		    padding-left: 10px;
+		    // font-weight: 700;
+		    .fs(18);
+		}
+		.fa-search{
+			position: absolute;
+			right:14px;
+			.fs(18);
+		}
 	}
 </style>
